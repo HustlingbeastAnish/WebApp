9439e21894cdf89cbe59d214454298caad4e1f38
Sumit Agarwal <92793917+sumit2002agarwal@users.noreply.github.com>
2022-12-08 17:17:39 +0530

    Update App.js
    added TeachStu in app.js

--- attend/src/App.js
+++ attend/src/App.js
@@ -1,16 +1,24 @@
+
+
 import Navbar from "./Components/Navbar/navbar";
 import Login from "./Components/LogIn/login";
+import TeachStu from "./Components/TeachStu/teachstu";
+
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
+   
       <BrowserRouter>
-        <Navbar />
+       
+      <Navbar />
         <Routes>
           <Route exact path="/login" element={<Login />}></Route>
         </Routes>
+        
       </BrowserRouter>
+      <TeachStu />
     </>
   );
 }

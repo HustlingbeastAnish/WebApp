2200dc986a21741582fc9c3437d9c9129b95a69e
Anish <anishkumarsingh45@gmail.com>
2022-12-11 02:14:32 +0530

    SignUp Functionality Successfull

---
+++ .history/backend/server/model/model_20221211005648.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+var Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    // required: true,
+  },
+  email: {
+    type: String,
+    // required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    // required: true,
+  },
+});
+const UserDB = mongoose.model("userdb", Schema);
+
+module.exports = UserDB;

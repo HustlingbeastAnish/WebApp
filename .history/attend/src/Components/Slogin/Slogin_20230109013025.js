4b0ef9ccdfb3fb63bbdf5988839cdfd8f5edb25e
Anish <anishkumarsingh45@gmail.com>
2023-01-09 02:36:57 +0530

    SideBar added

---
+++ .history/attend/src/Components/Slogin/Slogin_20230109013025.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SideNavbar from "../StudentSection/SideNavbar";
+
+function Slogin() {
+  return <SideNavbar />;
+}
+
+export default Slogin;
---
+++ .history/attend/src/Components/Slogin/Slogin_20230109013159.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SideNavbar from "../StudentSection/SideNavbar";
+
+function Slogin() {
+  return (
+    <div>
+      <div>
+        <h2>Welcome Student....</h2>
+      </div>
+      <SideNavbar />
+    </div>
+  );
+}
+
+export default Slogin;

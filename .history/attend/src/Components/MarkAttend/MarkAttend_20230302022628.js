163b8d8edfa489f3eaf16365a318bf78f41d8442
Anish <anishkumarsingh45@gmail.com>
2023-03-02 02:35:05 +0530

    toggle mode for present absent done

--- .history/attend/src/Components/MarkAttend/MarkAttend_20230302022628.js
+++ .history/attend/src/Components/MarkAttend/MarkAttend_20230302022628.js
@@ -90,13 +90,6 @@ function MarkAttend(props) {
         console.log(err);
       });
   };
-
-  const HandleAbsentees = (idx) => {
-    Setabsent((absent) => ({
-      ...absent,
-      [idx]: 1 - absent[idx],
-    }));
-  };
   return (
     <>
       <div className="bg-gray-700 border-black flex flex-col items-center">
@@ -219,7 +212,7 @@ function MarkAttend(props) {
                         />
                         <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
                         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                          {absent[idx] === 1 ? "Absent" : "Present"}
+                          {arr[idx] === 1 ? "Absent" : "Present"}
                         </span>
                       </label>
                     </div>

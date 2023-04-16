ae0d17b20f4c78966020016af5df96196ff6d323
Anish <anishkumarsingh45@gmail.com>
2023-04-16 19:17:02 +0530

    PORTS changged

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 8080;
 const connectDB = require("./server/database/connection");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
@@ -12,7 +12,7 @@ var cors = require("cors");
 
 app.use(cors());
 mongoose.set("strictQuery", true);
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./.env" });
 // app.set("veiw engine", "html");
 app.use(express.static("public"));
 app.use(express.json());
